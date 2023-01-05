a88e26c4d6bdbc1c1f1adf6fcd01eb6c98f73a73
Tristan <tristanlp.alfred@gmail.com>
2023-01-05 13:36:37 +0100

    feature/simple_api_initiation : creation of a simple API returning 200 to check if an API works correctly

--- MySoftwareInitiation/api_catalog/views.py
+++ MySoftwareInitiation/api_catalog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets, status
 
-# Create your views here.
+
+class CheckApi(viewsets.ViewSet):
+    """
+    View returning 200 to check if an API works correctly
+    """
+
+    def list(self):
+        """
+        Return a list of all users.
+        """
+        return Response(status=status.HTTP_200_OK)
+
